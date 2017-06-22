cbc00c6f37439e26f15f90d52acc3354707b9b91
Oleg Repin <oleg.repin@iqoption.com>
2017-06-22 15:53:14 +0300

    fix(ci): fix deploing docs

--- utils/documentation.js
+++ utils/documentation.js
@@ -3,12 +3,26 @@ const ghpages = require('gh-pages');
 
 const styleguidePath = path.join(__dirname, '..', 'styleguide');
 
-ghpages.publish(styleguidePath, {
+const props = {
   user: {
     name: 'Oleg Repin',
     email: 'oleg.repin@iqoption.com'
-  }
-}, err => {
+  },
+};
+
+if (process.env.GH_TOKEN) {
+  const packageRepo = require('../package').repository.url;
+
+  props.repo =
+    packageRepo.replace(
+      'github.com',
+      process.env.GH_TOKEN + '@github.com'
+    );
+}
+
+
+
+ghpages.publish(styleguidePath, props, err => {
   if (err) {
     console.error('Error when publishing docs!');
     console.error(err);
