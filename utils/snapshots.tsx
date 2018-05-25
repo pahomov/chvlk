556f93d44f956ae89cc1648e2f3f463a4e547b9d
Victor Fomenko <victorfomenko@me.com>
2018-05-25 16:18:40 +0300

    build(npm: react, enzyme, typescript): (#13)
    * build(npm: react, enzyme, typescript):
    
    * fix(package.json and enzyme adapters):
    
    * fix(snapshot):
    
    * refactor(change package.json):

--- utils/snapshots.tsx
+++ utils/snapshots.tsx
@@ -1,22 +1,27 @@
-import { shallow, ShallowWrapper } from 'enzyme';
+import { configure, shallow, ShallowWrapper } from 'enzyme';
+import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
 import * as React from 'react';
+
+configure({ adapter: new ReactSixteenAdapter() });
 
 export type IGetTarget = (
   tree: ShallowWrapper<any, any>
 ) => jest.Matchers<void>;
+
+interface ISnapshotInstance<P> {
+  props: P;
+  getTarget?: IGetTarget;
+}
 
 const defaultGetTarget: IGetTarget = (tree: ShallowWrapper<any, any>): any =>
   tree;
 
 export default function<P>(
   Component: React.ComponentClass<P>,
-  snapshots: Array<{
-    props: P;
-    getTarget?: IGetTarget;
-  }>
+  snapshots: Array<ISnapshotInstance<P>>
 ): void {
   test(`${Component.displayName} snapshots`, () => {
-    snapshots.forEach((snapshot: { props: P; getTarget: IGetTarget }): void => {
+    snapshots.forEach((snapshot: ISnapshotInstance<P>): void => {
       expect(
         (snapshot.getTarget || defaultGetTarget)(
           shallow(<Component {...snapshot.props} />)
